07dd7c424ab1b7a289c889f4602251754c21f364
Muhammed <muhammedihsansolak@gmail.com>
2023-07-04 11:41:44 +0300

    Generics

---
+++ m1-generics/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,26 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> items = new ArrayList<>();
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+
+        //items.add("Apple");
+
+        printDoubled(items);
+
+    }
+
+    private static void printDoubled(ArrayList<Integer> items) {
+        for (int each : items) {
+            System.out.println((Integer) each * 2);
+        }
+    }
+}
