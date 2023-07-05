5974042760f9523b023889a38cd908e7ef5d282b
Muhammed <muhammedihsansolak@gmail.com>
2023-07-05 11:25:26 +0300

    Generics

--- m1-generics/src/main/java/com/cydeo/Team.java
+++ m1-generics/src/main/java/com/cydeo/Team.java
@@ -3,7 +3,8 @@ package com.cydeo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Team <T extends Player> {//T should accept only: Soccer / Football / Baseball players
+public class Team <T extends Player> {
+    //T should accept only: Soccer / Football / Baseball players
     //it should extend the Player.java class for restricting the generic type
 
     private String name;
