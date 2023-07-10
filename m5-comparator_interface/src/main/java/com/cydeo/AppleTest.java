1ebeca9faa54397e004ba0ebc0dcc75129f15865
Muhammed <muhammedihsansolak@gmail.com>
2023-07-10 14:14:25 +0300

    comparator interface module created

---
+++ m5-comparator_interface/src/main/java/com/cydeo/AppleTest.java
@@ -0,0 +1,24 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.Comparator.comparing;
+
+public class AppleTest {
+    public static void main(String[] args) {
+        List<Apple> inventory = new ArrayList<>();
+        inventory.add(new Apple(300,Color.GREEN));
+        inventory.add(new Apple(150,Color.RED));
+        inventory.add(new Apple(350,Color.GREEN));
+        inventory.add(new Apple(180,Color.GREEN));
+
+        Comparator<Apple> sortApple = comparing( (Apple apple) -> apple.getWeight() );
+        inventory.sort(sortApple);
+        System.out.println(inventory);
+
+        inventory.sort( comparing(Apple::getWeight) );
+
+    }
+}
---
+++ m5-comparator_interface/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,30 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(10,0,15,5,20,50);
+        System.out.println(list);
+
+        Collections.sort(list);//ascending
+
+        System.out.println(list);
+
+        Collections.sort( list, new MyComparator() );//descending
+
+        Collections.sort(list , (o1,o2) -> (o1>o2)? -1:(o1<o2)? 1 : 0);
+
+        //***********************
+
+        list.sort( (o1, o2) -> o1.compareTo(o2) );//---> Ascending
+        System.out.println( list );
+
+        list.sort( (o2, o1) -> o1.compareTo(o2) );//----> Descending
+        System.out.println(list);
+
+    }
+}
