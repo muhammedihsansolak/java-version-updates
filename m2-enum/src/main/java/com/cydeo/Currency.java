534bd75668333eec92ebc6cd9d9095326237e6c4
Muhammed <muhammedihsansolak@gmail.com>
2023-07-10 10:32:25 +0300

    project updated

--- m2-enum/src/main/java/com/cydeo/Currency.java
+++ m2-enum/src/main/java/com/cydeo/Currency.java
@@ -15,4 +15,5 @@ public enum Currency {
     public int getValue() {
         return value;
     }
+
 }
--- m2-enum/src/main/java/com/cydeo/review/Test.java
+++ m2-enum/src/main/java/com/cydeo/review/Test.java
@@ -3,5 +3,7 @@ package com.cydeo.review;
 public class Test {
     public static void main(String[] args) {
         User employee = new User("Max","Muhammed","SOLAK",Role.EMPLOYEE);
+        System.out.println(Role.EMPLOYEE.ordinal());
     }
+
 }
