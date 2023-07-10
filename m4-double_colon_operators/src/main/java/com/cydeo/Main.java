3a25da53cd253d96790cadf3774978f09acfed1c
Muhammed <muhammedihsansolak@gmail.com>
2023-07-10 12:20:12 +0300

    double colon operator topic updated

--- m4-double_colon_operators/src/main/java/com/cydeo/Main.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -1,5 +1,8 @@
 package com.cydeo;
 
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Main {
@@ -23,6 +26,18 @@ public class Main {
         Calculator obj = new Calculator();
         Calculate calculate4 = obj::findMultiply; //we can call it through object
 
+
+        BiFunction<String, Integer, String> function = (str,i) -> str.substring(i);
+
+        BiFunction<String, Integer, String> function2 = String::substring;
+        System.out.println(function2.apply("Muhammed", 4));
+
+        Function<Integer,Double> function5 = new MyClass()::method;
+        BiFunction<MyClass,Integer,Double> function6 = MyClass::method;
+        //if we declare the type in parameter, we do not have to create an object for instance methods. we could call them through class name
+
+        Consumer<Integer> display = i -> System.out.println(i);
+        Consumer<Integer> display2 = System.out::println;
 
     }
 }
