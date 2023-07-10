e0f198afec86ca66857632eabad63e701066a279
Muhammed <muhammedihsansolak@gmail.com>
2023-07-10 11:55:40 +0300

    double colon operator topic started

--- m4-double_colon_operators/src/main/java/com/cydeo/Calculator.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
     public static void findSum(int x, int y){
         System.out.println("Sum: "+ (x+y) );
     }
-    public static void findMultiply(int x, int y){
+    public void findMultiply(int x, int y){
         System.out.println("Multiply: "+ (x*y) );
     }
 
--- m4-double_colon_operators/src/main/java/com/cydeo/Main.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -11,7 +11,18 @@ public class Main {
             System.out.println(a+b);
         };
 
-        Calculate calculate2 = (x,y) -> Calculator.findMultiply(x,y);//you can use method in lambda expression as long as the signatures match
+        Calculate calculate2 = (x,y) -> Calculator.findSum(x,y);
+        //you can use method in lambda expression as long as the signatures match
+        //YOU CAN SIMPLIFY IT WITH USING DOUBLE COLON OPERATOR:
+
+        Calculate calculate3 = Calculator::findSum; //just method name needed. no parameter
+
+        calculate3.calculate(10,20);
+
+        //How to use double colon operator for instance methods?
+        Calculator obj = new Calculator();
+        Calculate calculate4 = obj::findMultiply; //we can call it through object
+
 
     }
 }
