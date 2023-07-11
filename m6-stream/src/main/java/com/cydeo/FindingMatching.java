8b12b7dfb61c68fc6d9cc7666cf78c0da50284d0
Muhammed <muhammedihsansolak@gmail.com>
2023-07-11 19:49:31 +0300

    Stream module updated

---
+++ m6-stream/src/main/java/com/cydeo/FindingMatching.java
@@ -0,0 +1,66 @@
+package com.cydeo;
+
+import com.cydeo.dishTask.Dish;
+import com.cydeo.dishTask.DishData;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+public class FindingMatching {
+    public static void main(String[] args) {
+
+        //after these operation you can not have any other stream operations, because they all return boolean expression
+
+        //Checks if all the elements of the stream match the given predicate. returns boolean
+        //**** allMatch() ****
+
+        boolean result = DishData.getAll().stream().allMatch( p -> p.getCalories() < 1000 ); //if all the dish's calories less than 1000, return true
+        System.out.println(result);
+
+        System.out.println("-------------");
+
+        // Checks if there is an element in the stream matching the given predicate. returns boolean
+        //**** anyMatch() ****
+
+        if ( DishData.getAll().stream().anyMatch(Dish::isVegetarian) ) {
+            System.out.println("Menu is vegetarian friendly");
+        }
+
+        System.out.println("-------------");
+
+        //  Ensures no elements in the stream match the given predicate. returns boolean
+        //**** noneMatch() ****
+
+        boolean isHealthy = DishData.getAll().stream().noneMatch(p -> p.getCalories() >= 1000); //--> if none of them match with the given condition returns true
+        System.out.println(isHealthy);
+
+        System.out.println("-------------");
+
+        // Returns any element from a Stream. returns "Optional Interface" (is created for avoiding null pointer exceptions)
+        //**** findAny() ****
+
+        Optional<Dish> findAny = DishData.getAll().stream().filter(Dish::isVegetarian).findAny();
+        System.out.println(findAny.get());
+
+        //findAny() returns Optional type, if you want you can use get() method for original object type
+        //IF THE CONDITION IS TRUE, IT RETURNS ANY OF THE MATCHED ONES
+
+        System.out.println("-------------");
+
+        // Returns the first element from a Stream. returns "Optional Interface"
+        //**** findFirst() ****
+
+        Optional<Dish> findFirst= DishData.getAll().stream().filter(Dish::isVegetarian).findFirst();
+        System.out.println(findAny.get());
+
+        System.out.println("PARALLEL STREAMS (Async)");
+
+        System.out.println( IntStream.range(0,100).parallel().findAny() );
+        System.out.println( IntStream.range(0,100).parallel().findFirst() );
+
+        List<String> list1 = Arrays.asList("Jhonny","David","Jack","Duke","Jill","Dany","Julia","Jenish","Divya");
+        List<String> list2 = Arrays.asList("Jhonny","David","Jack","Duke","Jill","Dany","Julia","Jenish","Divya");
+    }
+}
---
+++ m6-stream/src/main/java/com/cydeo/pairTask/Main.java
@@ -0,0 +1,26 @@
+package com.cydeo.pairTask;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> list1 = Arrays.asList(1,2,3);
+        List<Integer> list2 = Arrays.asList(3,4);
+
+
+
+    }
+    //TODO
+}
+/*
+Given two lists of numbers, how would you return all pairs of numbers? For
+example, given a list [1,2,3] and a list [3,4] you should return [(1,3),(1,4),(2,3),
+(2,4),(3,3),(3,4)]
+• List<Integer> numbers1 = Arrays.asList(1,2,3);
+• List<Integer> numbers2 = Arrays.asList(3,4);
+• List<int[]> pairs = ?????
+
+ */
