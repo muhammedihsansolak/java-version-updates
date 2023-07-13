3bb2ea71d916facf58829833b89b8a6582ab43ee
Muhammed <muhammedihsansolak@gmail.com>
2023-07-13 10:23:27 +0300

    ReduceMethod and StreamOperations class updated

--- m6-stream/src/main/java/com/cydeo/ReduceMethod.java
+++ m6-stream/src/main/java/com/cydeo/ReduceMethod.java
@@ -35,5 +35,7 @@ public class ReduceMethod {
 
         System.out.println(totalCal);
 
+
+
     }
 }
--- m6-stream/src/main/java/com/cydeo/StreamOperations.java
+++ m6-stream/src/main/java/com/cydeo/StreamOperations.java
@@ -1,5 +1,8 @@
 package com.cydeo;
 
+import com.cydeo.dishTask.Dish;
+import com.cydeo.dishTask.DishData;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -26,8 +29,8 @@ public class StreamOperations { //Intermediate Operations
         //**** map() ****
 
         list.stream()
-                .filter(p -> p%2==0)
-                .map(p -> p*3)
+                .filter(p -> p % 2 == 0)
+                .map(p -> p * 3)
                 .forEach(System.out::println);
 
         System.out.println("===============");
@@ -45,8 +48,8 @@ public class StreamOperations { //Intermediate Operations
         //return a stream with length of given number
         //**** limit() ****
 
-        Stream<Integer> stream =  list.stream()
-                .filter(p->p>1)
+        Stream<Integer> stream = list.stream()
+                .filter(p -> p > 1)
                 .limit(2);
 
         stream.forEach(System.out::println);
@@ -57,9 +60,17 @@ public class StreamOperations { //Intermediate Operations
         //**** skip() ****
 
         list.stream()
-                .filter(p->p>1)
+                .filter(p -> p > 1)
                 .skip(2)
                 .forEach(System.out::println);
+
+        //returns how many data stream object has, return long
+        //**** count() ****
+
+        long count =
+                DishData.getAll().stream().count();
+
+        System.out.println(count);
 
     }
 }
